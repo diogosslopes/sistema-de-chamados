006048db1d0473526452016f87a7a1a7b6c145eb
Diogo Sousa Silva Lopes <67078434+diogosslopes@users.noreply.github.com>
2023-10-16 11:53:19 -0300

    Versão Beta 4.0

--- src/components/TasksTable.js
+++ src/components/TasksTable.js
@@ -121,9 +121,9 @@ export default function TasksTable({ tasks, order, getDoc, page }) {
                                 <td data-label="Prioridade">{task.priority}</td>
                                 <td data-label="Status"><span className="status">{task.status}</span></td>
                                 <td data-label="Criado em">{task.created}</td>
+                                {page === 'completedtasks' &&
                                 <td data-label="Concluido em">{task.concluded}</td>
-                                {/* {page === 'completedtasks' &&
-                                } */}
+                                }
                                 <td data-label="#">
                                     {page === 'completedtasks' ? 
                                         <button className="task-btn search" onClick={() => editClient('show', task)}><FiSearch size={17} /></button>
