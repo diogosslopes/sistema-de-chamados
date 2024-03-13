b9f0f63a79498552d0637403c4926af650be0ca2
Diogo Sousa Silva Lopes <diogodil@hotmail.com>
2024-03-13 13:13:19 -0300

    Editing config server

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(
         credentials: true,
         origin: '*',
         allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
-        methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+        methods: 'GET, OPTIONS, PATCH, DELETE, POST, PUT'
     }))
 app.use(express.json({
 
